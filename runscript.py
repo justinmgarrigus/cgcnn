e2100ac0ad746492bcf5cb8f4d167b57b1157d27
Angela Zhang <azhang2@cs.utexas.edu>
2024-06-18 20:27:00 -0500

    script

---
+++ runscript.py
@@ -0,0 +1,106 @@
+import subprocess
+
+# PART 1: cross validation test for a single type of model
+
+# k-fold cross validation
+for i in range(10): 
+    f = open("fromscratch_kfold_" + str(i) + ".txt", "w")
+    subprocess.popen("python3 main.py --optim SGD --freeze-fc 0 --h-fea-len 32 --n-conv 4 --cross_validation k-fold-cross-validation --cross_param 10 --counter " + str(i) + " --seed 42 root_dir")
+    f.close()
+
+# bootstrapping
+for i in range(100):
+    f = open("fromscratch_bootstrapping_" + str(i) + ".txt", "w")
+    subprocess.popen("python3 main.py --optim SGD --freeze-fc 0 --h-fea-len 32 --n-conv 4 --cross_validation bootstrapping --cross_param 100 --counter " + str(i) + " --seed 42 root_dir")
+    f.close()
+
+# leave 2 out
+for i in range(600*500):
+    f = open("fromscratch_leave2out_" + str(i) + ".txt", "w")
+    subprocess.popen("python3 main.py --optim SGD --freeze-fc 0 --h-fea-len 32 --n-conv 4 --cross_validation leave-p-out --cross_param 2 --counter " + str(i) + " --seed 42 root_dir")
+    f.close()  
+
+# leave 1 out
+for i in range(600):
+    f = open("fromscratch_leave1out_" + str(i) + ".txt", "w")
+    subprocess.popen("python3 main.py --optim SGD --freeze-fc 0 --h-fea-len 32 --n-conv 4 --cross_validation leave-one-out --counter " + str(i) + " --seed 42 root_dir")
+    f.close()
+
+# monte carlo
+for i in range(100):
+    f = open("fromscratch_montecarlo_" + str(i) + ".txt", "w")
+    subprocess.popen("python3 main.py --optim SGD --freeze-fc 0 --h-fea-len 32 --n-conv 4 --cross_validation monte-carlo --cross_param 100 --counter " + str(i) + " --seed 42 root_dir")
+    f.close()
+
+# PART 2: each model with a single cross validation type (k-fold)
+
+# file for pretraining
+fileName = "pre-trained/band-gap.pth.tar"
+
+# transfer learning
+for i in range(10): 
+    f = open("transfer_kfold_" + str(i) + ".txt", "w")
+    subprocess.popen("python3 main.py --optim SGD --freeze-embedding --freeze-conv --freeze-fc 0 --h-fea-len 32 --n-conv 4 --pretrain " + fileName + " --cross_validation k-fold-cross-validation --cross_param 10 --counter " + str(i) + " --seed 42 root_dir")
+    f.close()
+
+# transfer + 1 CNN
+for i in range(10): 
+    f = open("transfercnn_kfold_" + str(i) + ".txt", "w")
+    subprocess.popen("python3 main.py --optim SGD --freeze-embedding --freeze-conv --freeze-fc 0 --n-conv 5 --h-fea-len 32 --pretrain " + fileName + " --cross_validation k-fold-cross-validation --cross_param 10 --counter " + str(i) + " --seed 42 root_dir")
+    f.close()
+
+# transfer + 1 frozen fc + 1 added fc
+for i in range(10): 
+    f = open("transferfrozenadditional_kfold_" + str(i) + ".txt", "w")
+    subprocess.popen("python3 main.py --optim SGD --freeze-embedding --freeze-conv --freeze-fc 1 --n-h 2 --n-conv 4 --h-fea-len 32 --pretrain " + fileName + " --cross_validation k-fold-cross-validation --cross_param 10 --counter " + str(i) + " --seed 42 root_dir")
+    f.close()
+
+# transfer + 1 free fc + 1 added fc + cnn
+for i in range(10): 
+    f = open("transferfreeadditionalcnn_kfold_" + str(i) + ".txt", "w")
+    subprocess.popen("python3 main.py --optim SGD --freeze-embedding --freeze-conv --freeze-fc 0 --n-h 2 --n-conv 5 --h-fea-len 32 --pretrain " + fileName + " --cross_validation k-fold-cross-validation --cross_param 10 --counter " + str(i) + " --seed 42 root_dir")
+    f.close()
+
+# transfer + embedding free + fine-tuning
+for i in range(10): 
+    f = open("transferembeddingfinetuning_kfold_" + str(i) + ".txt", "w")
+    subprocess.popen("python3 main.py --optim SGD --freeze-conv --freeze-fc 0 --fine-tune --h-fea-len 32 --n-conv 4 --pretrain " + fileName + " --cross_validation k-fold-cross-validation --cross_param 10 --counter " + str(i) + " --seed 42 root_dir")
+    f.close()
+
+# transfer + embedding free
+for i in range(10): 
+    f = open("transferembedding_kfold_" + str(i) + ".txt", "w")
+    subprocess.popen("python3 main.py --optim SGD --freeze-conv --freeze-fc 0 --h-fea-len 32 --n-conv 4 --pretrain " + fileName + " --cross_validation k-fold-cross-validation --cross_param 10 --counter " + str(i) + " --seed 42 root_dir")
+    f.close()
+
+# transfer + embedding free + 1 add fc
+for i in range(10): 
+    f = open("transferembeddingfc_kfold_" + str(i) + ".txt", "w")
+    subprocess.popen("python3 main.py --optim SGD --freeze-conv --freeze-fc 0 --n-h 2 --h-fea-len 32 --n-conv 4 --pretrain " + fileName + " --cross_validation k-fold-cross-validation --cross_param 10 --counter " + str(i) + " --seed 42 root_dir")
+    f.close()
+
+# fine tuning + adam + 1 fc
+for i in range(10): 
+    f = open("finetuningadamfc_kfold_" + str(i) + ".txt", "w")
+    subprocess.popen("python3 main.py --optim Adam --freeze-fc 0 --n-h 2 --fine-tune --h-fea-len 32 --nconv 4 --pretrain " + fileName + " --cross_validation k-fold-cross-validation --cross_param 10 --counter " + str(i) + " --seed 42 root_dir")
+    f.close()
+
+# fine tuning + adam + 1 CNN
+for i in range(10): 
+    f = open("finetuningadamcnn_kfold_" + str(i) + ".txt", "w")
+    subprocess.popen("python3 main.py --optim Adam --freeze-fc 0 --fine-tune --h-fea-len 32 --nconv 5 --pretrain " + fileName + " --cross_validation k-fold-cross-validation --cross_param 10 --counter " + str(i) + " --seed 42 root_dir")
+    f.close()
+
+
+# fine tuning all free
+for i in range(10): 
+    f = open("finetuning_kfold_" + str(i) + ".txt", "w")
+    subprocess.popen("python3 main.py --optim SGD --freeze-fc 0 --fine-tune --h-fea-len 32 --n-conv 4 --pretrain " + fileName + " --cross_validation k-fold-cross-validation --cross_param 10 --counter " + str(i) + " --seed 42 root_dir")
+    f.close()
+
+# fine tuning + 1 add fc
+for i in range(10): 
+    f = open("finetuningfc_kfold_" + str(i) + ".txt", "w")
+    subprocess.popen("python3 main.py --optim SGD --freeze-fc 0 --n-h 2 --fine-tune --h-fea-len 32 --n-conv 4 --pretrain " + fileName + " --cross_validation k-fold-cross-validation --cross_param 10 --counter " + str(i) + " --seed 42 root_dir")
+    f.close()
+
