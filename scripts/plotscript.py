b757f3518c3b738421b56a454a07ab5066b0d558
Angela Zhang <angelalzhang@gmail.com>
2024-07-15 14:17:51 -0500

    more changes to the plotting script

--- scripts/plotscript.py
+++ scripts/plotscript.py
@@ -5,16 +5,25 @@ import os
 import pandas as pd
 import numpy as np
 from matplotlib.ticker import MultipleLocator
+from statistics import mean
 
-def extract_loss_from_file(filename):
-    losses = []
+def extract_final_MAE(filename):
+    mae_values = []
+    completed = False
     with open(filename, 'r') as file:
-            for line in file:
-                if 'Loss' in line:
-                    afterloss = line.split('Loss')[1]
-                    loss = float(afterloss.split()[0].strip())
-                    losses.append(loss)
-    return losses[loss-1]
+        for line in file:
+            if '* MAE' in line:
+                mae = float(line.split('* MAE')[1].strip())
+                mae_values.append(mae)
+                # afterloss = line.split('Loss')[1]
+                # loss = float(afterloss.split()[0].strip())
+                # losses.append(loss)
+            if 'NO TEST' in line:
+                completed = True
+    if not completed:
+        return -1.0
+    else:
+        return mae_values[len(mae_values) - 1]
 
 def extract_mae_from_file(filename):
     mae_values = []
@@ -25,7 +34,7 @@ def extract_mae_from_file(filename):
                 mae_values.append(mae)
     return mae_values
 
-def plot_loss_over_things():
+def plot_mae_over_functions(variables, typemodel):
 
     #if the loss exists in the file, average over all k folds
     #make sure the optimizer is different
@@ -40,7 +49,36 @@ def plot_loss_over_things():
     marker_size = 11  # Increase marker size
 
     for (title, directory_list), marker, linestyle in zip(variables.items(), marker, linestyle):
-        files = [[]]
+        all_mae_values = []
+        
+        #gather all files that have the name 'finetune', 'from scratch', etc, create a 2d array of size X type
+        for j in range(len(directory_list[1]) + 1): #iterating over the different sizes of the thing we are running
+            mae_per_size = []
+            for l in range(6): #iterating over the six different types of model used
+                if j == len(directory_list): #default value
+                    root = directory_list[0] + '/' + str(directory_list[2])
+                else:
+                    root = directory_list[0] + '/' +  str(directory_list[1][j])
+                files = glob.glob(typemodel[l], root_dir = root)
+                
+                avgmaelist = []
+                for file in files:
+                    mae = extract_final_MAE(file)
+                    if mae != -1.0:
+                        avgmaelist.append(mae)
+                
+                maefinal = mean(avgmaelist)
+                mae_per_size.append(maefinal)
+            all_mae_values.append(mae_per_size)
+        
+            
+
+
+
+
+                
+            
+
         path = directory_list[0] + '/' + str(directory_list[1])
         files.append(list(os.scandir(path)))
         #files.extend(os.scandir(path))
@@ -60,9 +98,9 @@ def plot_loss_over_things():
     # Increase tick label sizes for x and y axis numbers
     ax.tick_params(axis='both', which='major', labelsize=20)
 
-    # plt.tight_layout()
-    # plt.savefig(output_filename, dpi=300)
-    # plt.show()
+    plt.tight_layout()
+    plt.savefig(output_filename, dpi=300)
+    plt.show()
 
     return
 
@@ -118,6 +156,15 @@ variables = {
     'Training Size': ['tests/trainsize', [100, 200, 300, 400, 500, 600], 'full'], 
     'Weight Decay': ['tests/weightdecay', [0.1, 0.01, 0.001, 0.0001], 0]
 }
+
+typemodel = [
+    'FromScratch_k_fold_*.txt',
+    'FreezeConv_k_fold_*.txt',
+    'FreezeConvEmbed_kfold_*.txt',
+    'FineTune_kfold_*.txt',
+    'FreezeConv-FineTune_kfold_*.txt',
+    'FreezeConvEmbed-FineTune_kfold_*.txt'
+]
 
 # Define the patterns for the sets of files
 patterns = {
